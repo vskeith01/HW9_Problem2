d253d7d2d029e1acaaa1a7e0e76e8a4293380e09
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-28 01:27:39 +0000

    idea to sort matrix, unsuccessful

--- .ccls-cache/@home@runner@HW9Problem2/main.cpp
+++ .ccls-cache/@home@runner@HW9Problem2/main.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 
 /////////////// Function Prototypes ////////////
-void average(int matrix[][7], int row);
-void printMatrix7(int matrix[][7], int N_ROWS, int N_COLUMNS);
-void weightedAverage(int matrix[][7], int row);
+void average(int matrix[][8], int row);
+void printMatrix8(int matrix[][8], int N_ROWS, int N_COLUMNS);
+void weightedAverage(int matrix[][8], int row);
+void selectionSort(int matrix[][8], int row);
+void copyMatrix8(int OGmatrix[][8], int NewMatrix[][8]);
+void DroppedAverage(int matrix[][8], int row);
 
 int main() {
 
 
-  int grades [][7]{
+  int grades [][8]{
                   {1,100,100,100,100},
                   {2,100,0,100,0},
                   {3,82,94,73,86},
@@ -21,10 +24,11 @@ int main() {
   for (int row = 0; row < 5; row++){
     average(grades, row);
     weightedAverage(grades, row);
+    DroppedAverage(grades, row);
     }
   
 
-  printMatrix7(grades, 5, 7);
+  //printMatrix8(grades, 5, 8);
 
   return 0;
 }
@@ -34,7 +38,7 @@ int main() {
 
 /* Function that calculates average for one row*/
 
-void average(int matrix[][7], int row)
+void average(int matrix[][8], int row)
 {
 
   double ave;
@@ -46,7 +50,7 @@ void average(int matrix[][7], int row)
 }
 
 /* This function calculates the weighted average for one row */
-void weightedAverage(int matrix[][7], int row)
+void weightedAverage(int matrix[][8], int row)
 {
 
   double W_ave;
@@ -58,8 +62,8 @@ void weightedAverage(int matrix[][7], int row)
 }
 
 
-/* This function prints a matrix with 7 collums*/
-void printMatrix7(int matrix[][7], int N_ROWS, int N_COLUMNS)
+/* This function prints a matrix with 8 collums*/
+void printMatrix8(int matrix[][8], int N_ROWS, int N_COLUMNS)
 {
     int row, col;
     for (row = 0; row < N_ROWS; row++)
@@ -70,5 +74,61 @@ void printMatrix7(int matrix[][7], int N_ROWS, int N_COLUMNS)
         cout << endl;    
     }
 
+  return;
+}
+
+/* this function sorts one row of the grades only in the matrix */
+
+void selectionSort(int matrix[][8], int row)
+{
+
+  int index, smallestIndex, loc, temp;
+
+  for (index = 1; index < (4 - 1); index++)
+    {
+      // increment through main array
+      smallestIndex = index;
+
+      // incrament through each comparison
+      for (loc = index + 1; loc < 4; loc++){
+        if (matrix[row][index] < matrix[row][smallestIndex])
+          smallestIndex = loc;    // collums of new smallest index for this row
+        }
+
+      // make swap
+      temp = matrix[row][smallestIndex];
+      matrix[row][smallestIndex] = matrix[row][index];
+      matrix[row][index] = temp;
+    }
+  
+  return;
+}
+
+/* This function copies a matrix */
+void copyMatrix8(int OGmatrix[][8], int NewMatrix[][8])
+{
+
+  for (int row = 0; row < 5; row++){
+    for (int col = 0; col < 8; col++){
+      NewMatrix[row][col] = OGmatrix[row][col];
+    }
+  }
+
+  return;
+}
+
+/* This function calculates the average without the lowest value*/
+void DroppedAverage(int matrix[][8], int row)
+{
+
+  int tempMatrix[][8]{};
+
+  copyMatrix8(matrix, tempMatrix);
+
+  selectionSort(tempMatrix, row);
+
+  printMatrix8(tempMatrix, 5, 8);
+
+  
   return;
 }
